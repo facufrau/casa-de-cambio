f9086164f80fa22572ce6391e02be8cd7fa1fb04
Facundo Frau <facufrau@gmail.com>
2021-10-04 11:48:40 -0300

    Agrega URL de api, modifica estilo y html

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ const $resultados = document.getElementById("api-result");
 const $botonConsultar = document.getElementById("boton-consultar");
 const $fecha = document.getElementById("elegir-fecha");
 const $monedaBase = document.getElementById("elegir-moneda");
+const API_URL = "https://api.exchangerate.host/";
+$botonConsultar.onclick = consultarAPI;
 
-$botonConsultar.onclick = enviarFormulario;
+function consultarAPI() {
+    const fecha = $fecha.value;
+    const monedaBase = $monedaBase.value;
 
-function enviarFormulario() {
-    console.log($fecha.value);
-    console.log($monedaBase.value);
+    fetch(`${API_URL}${fecha}?base=${monedaBase}`)
 }
