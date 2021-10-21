0c4277c6c8480705e2c02353cc00f5feda1e6f29
Facundo Frau <facufrau@gmail.com>
2021-10-21 19:30:07 -0300

    Agrega funciones para botones de seleccionar / desmarcar todos
    Para simplificar la experiencia del usuario se agregan 2 funciones que permiten seleccionar y deseleccionar todas las monedas mostradas.

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ const $cotizaciones = document.getElementById("lista-cotizaciones");
 const $botonConsultar = document.getElementById("boton-consultar");
 const $fecha = document.getElementById("elegir-fecha");
 const $monedaBase = document.getElementById("elegir-moneda");
+const $botonSeleccionarTodos = document.getElementById("seleccionar-todos");
+const $botonDesmarcarTodos = document.getElementById("desmarcar-todos");
 
 const API_URL = "https://api.exchangerate.host/";
 $botonConsultar.onclick = mostrarCotizacion;
+
+$botonSeleccionarTodos.onclick = seleccionarTodos;
+$botonDesmarcarTodos.onclick = desmarcarTodos;
 
 function mostrarCotizacion() {
     limpiarCotizacionAnterior($cotizaciones);
@@ -41,4 +46,14 @@ function limpiarCotizacionAnterior(nodoCotizaciones) {
         nodoCotizaciones.removeChild(nodoCotizaciones.firstChild);
     }
  }
- 
+
+
+function seleccionarTodos() {
+    const listaCheckboxes = document.querySelectorAll('input[type="checkbox"]');
+    listaCheckboxes.forEach(item => {item.checked = true});
+}
+
+function desmarcarTodos() {
+    const listaCheckboxes = document.querySelectorAll('input[type="checkbox"]');
+    listaCheckboxes.forEach(item => {item.checked = false});
+}
