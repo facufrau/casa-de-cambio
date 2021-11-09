e3a031688bc60b66c592ed57e946816c104ae5af
Facundo Frau <51481449+facufrau@users.noreply.github.com>
2021-11-09 13:31:23 -0300

    Agrega funcionalidad de elegir que cotizaciones mostrar de una lista con checkboxes

--- src/main.js
+++ src/main.js
@@ -4,18 +4,19 @@ const $fecha = document.getElementById("elegir-fecha");
 const $monedaBase = document.getElementById("elegir-moneda");
 const $botonSeleccionarTodos = document.getElementById("seleccionar-todos");
 const $botonDesmarcarTodos = document.getElementById("desmarcar-todos");
+const $botonFiltrar = document.getElementById("filtrar");
 
 const API_URL = "https://api.exchangerate.host/";
 $botonConsultar.onclick = mostrarCotizacion;
 
 $botonSeleccionarTodos.onclick = seleccionarTodos;
 $botonDesmarcarTodos.onclick = desmarcarTodos;
+$botonFiltrar.onclick = filtrarResultados; 
 
 function mostrarCotizacion() {
     limpiarCotizacionAnterior($cotizaciones);
     consultarAPI();
 }
-
 
 function consultarAPI() {
     const fecha = $fecha.value;
@@ -28,10 +29,10 @@ function consultarAPI() {
             {   
                 agregarElementoLista(moneda, respuestaJSON.rates[moneda]);
             })
+            filtrarResultados();
         })
         .catch(error => console.error("Error consultando la API: ", error));
 }
-
 
 function agregarElementoLista(moneda, cotizacion) {
     const elementoLista = document.createElement("li");
@@ -40,13 +41,11 @@ function agregarElementoLista(moneda, cotizacion) {
     $cotizaciones.appendChild(elementoLista);
 }
 
-
 function limpiarCotizacionAnterior(nodoCotizaciones) {
     while (nodoCotizaciones.firstChild) {
         nodoCotizaciones.removeChild(nodoCotizaciones.firstChild);
     }
  }
-
 
 function seleccionarTodos() {
     const listaCheckboxes = document.querySelectorAll('input[type="checkbox"]');
@@ -56,4 +55,30 @@ function seleccionarTodos() {
 function desmarcarTodos() {
     const listaCheckboxes = document.querySelectorAll('input[type="checkbox"]');
     listaCheckboxes.forEach(item => {item.checked = false});
+}
+
+function obtenerCotizacionesSeleccionadas() {
+    const listaCheckboxes = document.querySelectorAll('input[type="checkbox"]');
+    const monedasElegidas = [];
+    listaCheckboxes.forEach(item => {
+        if (item.checked) {
+            monedasElegidas.push(item.value);
+        }
+    });
+    
+    return monedasElegidas;
+}
+
+function filtrarResultados(){
+    const monedasElegidas = obtenerCotizacionesSeleccionadas();
+    const elementosMostrados = document.querySelectorAll("li");
+    elementosMostrados.forEach(elemento => {
+        let simbolo = elemento.textContent.slice(0,3);
+        if (!(monedasElegidas.includes(simbolo))) {
+            elemento.classList.add("oculto");
+        }
+        else {
+            elemento.classList.remove("oculto");
+        }
+    })
 }
